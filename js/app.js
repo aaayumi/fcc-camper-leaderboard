f7f5e6a4097d9ef16cb947f9ac231e0129ef6bf5
Ayumi Saito <ayumisaito27@gmail.com>
2017-05-13 10:59:43 +0200

    upload

--- js/app.js
+++ js/app.js
@@ -1,35 +1,81 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class LeaderBoard extends React.Component {
+    handleChange(e){
+        this.props.handleData(e.target.id);
+    }
+    render() {
+        return (
+        <div id="board">
+        <table>
+        <thead>
+        <tr>
+            <th> # </th>
+            <th> Camper Name </th>
+            <th id="recent" onClick={this.handleChange.bind(this)}> Points in the past 30days </th>
+            <th id="alltime" onClick={this.handleChange.bind(this)}> All time points </th>
+        </tr>
+        </thead>
+        <tbody>
+        { this.props.data.map( function(user, index){
+            return(
+            <tr key={ user.username }>
+            <td>#{index +1}</td>
+            <td><a href={'https://www.freecodecamp.com/' + user.username}><img src={user.img} className="profileImg"/><p>{ user.username }</p></a> 
+            </td>
+            <td>{ user.recent }</td>
+            <td>{ user.alltime }</td>
+            </tr>  
+            )
+        })
+    }
+    </tbody>
+    </table>
+    </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        //assign state itself, and a default value for items
+        this.state = {
+            item: []
+        };
+    
+    
+    // bind data 
+    this.handleData = this.handleData.bind(this);
+    }
+    // set up 'alltime' as a default
+    componentDidMount(){
+        this.handleData('alltime');
+        
+    }
+    handleData(key) {
+        axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/' + key)
+        .then((response) => {
+          this.setState({
+              item: response.data
+          });
+        })
+        .catch((err) => {
+            console.log(err.response.data)
+        })
+    }
+        
+    render() {
+        return (
+        <div>
+        <LeaderBoard data={this.state.item} />
+        </div>
+        )
+    }
+    }
 
 
-var LeaderBoard = React.createClass({
-	render: function () {
-		return (
-		 <table>
-		   <thead>
-		   <tr>
-		     <th>Ranking</th>
-		     <th>Camper</th>
-		     <th>Points in last 30 days</th>
-		     <th>Total Points</th>
-		    </tr>
-		    </thead>
-		    <tbody>item</tbody>
-		    </table>
-			);
-	}
-});
-
-var App = React.createClass({
-	render: function () {
-		return (
-			<div>
-             <h1> Camper LeaderBoard </h1>
-             <LeaderBoard />
-			 </div>
-			 );
-	}
-});
-
-ReactDOM.render(<App />, document.getElementById("app"));
-
+ReactDOM.render(<App/>, document.getElementById('app'));
